141f7a87ebecc3aa4ab2edbf29c43199387f1f8d
StudentDemo <patilkirans1995@gmail.com>
2024-03-13 14:57:18 +0530

    java commit day1

---
+++ src/CoreJava_DailyCommit/Variables.java
@@ -0,0 +1,18 @@
+package CoreJava_DailyCommit;
+
+public class Variables {
+
+	//Variables =>block of memory to store value
+	
+	int a=10;
+	int b=20;
+
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
