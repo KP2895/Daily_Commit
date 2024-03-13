00ffad46b6ce6e81e0244bbb6ca1103313df96f4
StudentDemo <patilkirans1995@gmail.com>
2024-03-13 16:59:09 +0530

    Added selenium jar files and chrome driver

--- src/CoreJava_DailyCommit/Variables.java
+++ src/CoreJava_DailyCommit/Variables.java
@@ -2,16 +2,38 @@ package CoreJava_DailyCommit;
 
 public class Variables {
 
-	//Variables =>block of memory to store value
+	//Variable =>block of memory to store value
+	//literals =>Value which we assign to variables
 	
-	int a=10;
-	int b=20;
-
+	int a=10; //global valriables
+	static int b=20;
 	
 	
-	
+	static void show() {
+		
+		String s1="Kiran"; //local varibale
+		System.out.println("Inside static method");
+		System.out.println(s1);
+		
+	}
+	void declare() {
+		
+		
+		String str="NewString";
+		System.out.println("Local variable:" +str);  //local varibales
+	}
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
+		System.out.println("In Main method");
+		
+		Variables v1=new Variables();
+		v1.declare();
+		show();
+		System.out.println("calling static variable:" +b);
+		System.out.println("calling static varibale:"+Variables.b);
+		
+		System.out.println("calling instance variable :"+v1.a);
+		
 
 	}
 
--- src/Selenium_DailyCommit/SeleniumBasics.java
+++ src/Selenium_DailyCommit/SeleniumBasics.java
@@ -1,8 +1,21 @@
 package Selenium_DailyCommit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumBasics {
 
+	
+	
 	public static void main(String[] args) {
+
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		
+		
 		
 	}
 
