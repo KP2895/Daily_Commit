3abce839a64155be227d806759d35ac17295ac4c
StudentDemo <patilkirans1995@gmail.com>
2024-03-16 00:56:10 +0530

    16/3/24

--- src/Selenium_DailyCommit/actionClass.java
+++ src/Selenium_DailyCommit/actionClass.java
@@ -45,13 +45,13 @@ public class actionClass {
 		System.out.println(eb2.getText());
 		
 //		3.Enter letters in capital 
-		Thread.sleep(2000);
+		
 		WebElement eb3=driver.findElement(By.cssSelector("input[name=\"q\"]"));
 		act.moveToElement(eb3).click().keyDown(Keys.SHIFT).sendKeys("Redmi Phones").build().perform();
 		
 		
 //		4.double click
-		Thread.sleep(2000);
+		
 		WebElement wb4=driver.findElement(By.cssSelector("div[class='_16ZfEv']"));
 		act.moveToElement(wb4).doubleClick().build().perform();
 		
@@ -60,31 +60,24 @@ public class actionClass {
 		
 //		5.Drag & drop
 		
-		driver.get("https://www.globalsqa.com/demo-site/draganddrop/#Photo%20Manager");
-		WebElement wb5=driver.findElement(By.cssSelector("li[id='Accepted Elements']"));
-		(wb5).click();
-	
+		driver.get("https://jqueryui.com/droppable/");
 		
+		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[class='demo-frame']")));
 		
-		
-		
-		try {
-			WebElement wb6 =driver.findElement(By.id("draggable"));
+	Thread.sleep(2000);
+			WebElement wb6 =driver.findElement(By.cssSelector("div[id='draggable']"));
+			System.out.println(wb6.getText());
 			
-			WebElement wb7=driver.findElement(By.id("droppable"));
-			
+			WebElement wb7=driver.findElement(By.cssSelector("div[id='droppable']"));
+			System.out.println(wb7.getText());
 			
 			act.dragAndDrop(wb6, wb7).build().perform();
 			
-			}
-			catch(Exception e) {
-			  System.out.println("Getting Error =>Need to check");
-			}
-		
+			
 		
 	}
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException  {
 
 		actionClass ac = new actionClass();
 		ac.ChildMain();
