f44cf5c218a41bfdb5cb9a9da1994decc845c2df
StudentDemo <patilkirans1995@gmail.com>
2024-03-16 12:37:05 +0530

    commit for 16/3/24

---
+++ src/Selenium_DailyCommit/iFrameDemo.java
@@ -0,0 +1,63 @@
+package Selenium_DailyCommit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class iFrameDemo {
+
+
+		WebDriver driver=null;
+	
+		void SetProperty() {
+
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+			
+			 driver =new ChromeDriver();
+			
+		} 
+		
+		void IframeHandling() {
+			
+			SetProperty();
+			
+			driver.get("https://jqueryui.com/droppable/");
+			driver.manage().window().maximize();
+			
+			Actions sc=new Actions(driver);
+			
+			WebElement sw=driver.findElement(By.cssSelector("iframe[class='demo-frame']"));
+			driver.switchTo().frame(sw);
+			
+			WebElement drag=driver.findElement(By.cssSelector("div#draggable"));
+			WebElement drop=driver.findElement(By.cssSelector("div#droppable"));
+			sc.dragAndDrop(drag, drop).build().perform();
+			
+			//to switch to main frame=>
+			driver.switchTo().defaultContent();
+			driver.findElement(By.xpath("//a[text()='Button']")).click();
+
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+	
+	
+	public static void main(String[] args) {
+		iFrameDemo fr=new iFrameDemo();
+		fr.IframeHandling();
+		
+		
+
+	}
+
+}
